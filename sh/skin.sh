a62aa333aaf2f524c7a6fea9376b777396c1e9d8
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-03-15 17:55:19 +0000

    Prettified errors, and LightningCSS warns.

--- sh/skin.sh
+++ sh/skin.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-lightningcss --bundle ${2:--m --sourcemap} $3 "css/${1}/index.css" -o "dist/${1}.css"
+if [ -f "$(which lightningcss)" ]; then
+	lightningcss --bundle ${2:--m --sourcemap} $3 "css/${1}/index.css" -o "dist/${1}.css"
+else
+	echo -e "\033[1;31mError\033[0m: LightningCSS is not available."
+fi
 exit
